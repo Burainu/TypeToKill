db15ba8ff7c2fdb668c4a8dc9daa3531cf6e950b
mchen1005 <mhchen1005@gmail.com>
2020-11-18 23:40:28 -0500

    Implenented enemy movement
    Added enemy movement

---
+++ Typing Game/Assets/Scripts/EnemyControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControll : MonoBehaviour
+{
+    public int hp;
+    public Transform castle;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1f;
+        castle = GameObject.FindGameObjectWithTag("Castle").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, castle.position, speed * Time.deltaTime);
+    }
+}
